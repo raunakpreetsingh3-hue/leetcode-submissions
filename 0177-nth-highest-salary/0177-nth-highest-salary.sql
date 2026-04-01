5461af5303224605ee8d5c11dddeb864c0558036
Rounak singh <raunakpreetsingh3@gmail.com>
2026-04-01 14:28:48 +0530

    Time: 431 ms (50.18%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -3,12 +3,11 @@ BEGIN
   SET N=N-1;
   RETURN (
       # Write your MySQL query statement below.
-      SELECT IFNULL(
           (
               SELECT DISTINCT salary
               FROM employee
               ORDER BY salary DESC
               LIMIT 1 OFFSET N
-          ),NULL)
+          )
   );
 END
