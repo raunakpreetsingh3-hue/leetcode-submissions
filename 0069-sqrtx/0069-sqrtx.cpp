b7e1ee6cd8cdb8cfbe737b21cc35669f956377b1
Rounak singh <raunakpreetsingh3@gmail.com>
2026-04-01 14:36:24 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.76%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long low=1;
+        long long high=x;
+        int ans=0;
+        while(low<=high){
+            long long mid=(low+high)/2;
+            if(mid*mid<=x){
+                ans=mid;
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+        return ans;
+    }
+};
