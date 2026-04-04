74368f1ba575f946b4814874be91c0238d38931b
Rounak singh <raunakpreetsingh3@gmail.com>
2026-04-04 12:24:53 +0530

    Time: 0 ms (100%), Space: 9.4 MB (7.62%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -4,7 +4,7 @@ public:
         if(s==goal) return true;
         if(s.size()!=goal.size()) return false;
         int n=s.size();
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             string b=s.substr(i)+s.substr(0,i);
             if(b==goal) return true;
         }
