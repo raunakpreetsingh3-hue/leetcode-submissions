7baeef0d34d348ac32267af6d015892cae0edc1a
Rounak singh <raunakpreetsingh3@gmail.com>
2026-04-04 12:02:34 +0530

    Time: 0 ms (100%), Space: 9.5 MB (27.24%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -6,8 +6,11 @@ public:
         unordered_map<char,char>mp1;
         unordered_map<char,char>mp2;
         for(int i=0;i<n;i++){
-            if(mp1.find(s[i])!=mp1.end() || mp2.find(t[i])!=mp2.end()){
-                if(mp1[s[i]]!=t[i] || mp2[t[i]]!=s[i]) return false;
+            if(mp1.find(s[i])!=mp1.end()){
+                if(mp1[s[i]]!=t[i]) return false;
+            }
+            if(mp2.find(t[i])!=mp2.end()){
+                if(mp2[t[i]]!=s[i]) return false;
             }
             mp1[s[i]]=t[i];
             mp2[t[i]]=s[i];
