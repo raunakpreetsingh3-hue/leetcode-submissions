7f4284b4fdc53b1937f2286aa4083154e0453cf5
Rounak singh <raunakpreetsingh3@gmail.com>
2026-04-07 10:50:27 +0530

    Time: 0 ms (100%), Space: 9.9 MB (53.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -5,8 +5,8 @@ public:
         int j=s.size()-1;
         while(i<=j){
             if(!isalnum(s[i])) i++;
-            if(!isalnum(s[j])) j--;
-            if(isalnum(s[i]) && isalnum(s[j])){
+            else if(!isalnum(s[j])) j--;
+            else if(isalnum(s[i]) && isalnum(s[j])){
                 if(tolower(s[i])==tolower(s[j])){
                     i++;
                     j--;
